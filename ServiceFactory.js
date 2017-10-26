0a97e142ac0621639bb66b95ab01ea43ebfd949d
Jochen Hörmann <jochenh@jochenh-laptop>
2017-10-26 19:35:17 +0200

    modify ServiceFactory

--- ServiceFactory.js
+++ ServiceFactory.js
@@ -3,28 +3,25 @@
 
 var log = require("rf-log");
 
-module.exports.start = function(options, next) {
+var self = this;
+self.services = {};
 
-   var self = this;
-   self.services = {};
-   
-   self.get = function(name) {
-      return (self.services[name] ? self.services[name] : null);
-   };
-
-   self.set = function(name, value) {
-      self.services[name] = value;
-      return self;
-   };
-
-   self.registerFunction = function(newFunction){
-      var newFunctionName = newFunction.name;
-      if (!self[newFunctionName]) {
-         self.prototype[newFunctionName] = newFunction;
-      } else {
-         log.critical("tried to register function " + newFunctionName + " in API, it but already exists.");
-      }
-   };
-
-   next();
+self.get = function(name) {
+   return (self.services[name] ? self.services[name] : null);
 };
+
+self.set = function(name, value) {
+   self.services[name] = value;
+   return self;
+};
+
+self.registerFunction = function(newFunction){
+   var newFunctionName = newFunction.name;
+   if (!self[newFunctionName]) {
+      self[newFunctionName] = newFunction;
+   } else {
+      log.critical("tried to register function " + newFunctionName + " in API, it but already exists.");
+   }
+};
+
+module.exports = self;
