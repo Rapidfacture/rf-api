f9c4ebea43b797f495838e0f9139dc384ca1e903
Felix <ff@rapidfacture.com>
2017-11-01 17:56:35 +0100

    update

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,6 @@
 
 module.exports = function (grunt) {
    grunt.loadNpmTasks('grunt-eslint')
-   grunt.loadNpmTasks('grunt-jsdoc-to-markdown')
 
    grunt.registerTask('default', ['eslint', 'jsdoc2md'])
 
@@ -14,15 +13,7 @@ module.exports = function (grunt) {
             configFile: '.eslintrc'
          },
          target: ['index.js']
-      },
-
-      jsdoc2md: {
-         oneOutputFile: {
-            src: '*.js',
-            dest: 'README.md'
-         }
       }
-
 
    })
 }
